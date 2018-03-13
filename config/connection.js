3eb0b25e6b2dda2110768dd502b2f687f7caeeac
Patrick Knight <ptknight00@gmail.com>
2018-03-12 22:25:04 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(err => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`)
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 module.exports = connection;
